ce495fea393efbd250594c35d5ef4f59296deb7d
Yitiao <1912468701@qq.com>
2024-02-27 16:01:36 +0800

    added speech_to_text.py and unzipped the images

---
+++ speech_to_text.py
@@ -0,0 +1,13 @@
+# `pip3 install assemblyai` (macOS)
+# `pip install assemblyai` (Windows)
+
+import assemblyai as aai
+
+
+def speech_to_text(file):
+    aai.settings.api_key = "0e6b8b15c65e49afbb439469bb6da09a"
+    transcriber = aai.Transcriber()
+
+    transcript = transcriber.transcribe(file)
+
+    return transcript
