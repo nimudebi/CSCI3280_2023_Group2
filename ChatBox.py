f9b88c0b73a50ec6e586a93c8f0a73411e87c1da
Amy-happy <130580513+Amy-happy@users.noreply.github.com>
2024-04-07 14:42:22 +0800

    Update ChatBox.py

--- ChatBox.py
+++ ChatBox.py
@@ -27,6 +27,63 @@ class ChatBox(QMainWindow):
         self.client=client
         self.server=server
 
+    # add close_voice function
+        self.ui.pushButton.clicked.connect(self.close_voice)
+
+        # add muting function
+        self.ui.pushButton_3.clicked.connect(self.muting)
+
+        # add voice change menu
+        self.menu = QMenu()
+        self.option1 = QAction("girl")
+        self.option1.triggered.connect(self.girl)
+        self.menu.addAction(self.option1)
+        self.option2 = QAction("boy")
+        self.option2.triggered.connect(self.boy)
+        self.menu.addAction(self.option2)
+        self.option3 = QAction("horror")
+        self.option3.triggered.connect(self.horror)
+        self.menu.addAction(self.option3)
+        self.option4 = QAction("echo")
+        self.option4.triggered.connect(self.echo)
+        self.menu.addAction(self.option4)
+        self.option5 = QAction("none")
+        self.option5.triggered.connect(self.none)
+        self.menu.addAction(self.option5)
+        self.ui.toolButton.setMenu(self.menu)
+        self.ui.toolButton.setPopupMode(QToolButton.InstantPopup)
+        self.boy_open = False
+        self.girl_open = False
+        self.mute = False
+        self.close_audio = False
+
+    def close_voice(self):
+        self.close_audio = not self.close_audio
+        # self.close_audio = True
+
+    def muting(self):
+        self.mute = not self.mute
+        # self.mute = True
+
+    def boy(self):
+        self.girl_open = False
+        self.boy_open = True
+
+    def girl(self):
+        self.boy_open = False
+        self.girl_open = True
+
+    def none(self):
+        self.boy_open = False
+        self.girl_open = False
+
+    def horror(self):
+        pass
+
+    def echo(self):
+        pass
+
+
     def closeEvent(self, event):
         try:
             if self.server:
